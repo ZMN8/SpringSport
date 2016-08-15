7f40f2732374fa432092e84169aeefa35dfdb0e6
zmn <1033711326>
2016-08-15 10:11:58 +0800

    更新代码

--- js/js.js
+++ js/js.js
@@ -1,13 +1,25 @@
 /**
  * Created by Administrator on 2016/8/10.
  */
-
+//sro();
+//function sro(){
+//    var Top=document.body.scrollTop;
+//    console.log(Top);
+//    if(Top<390){
+//        $(".slide_nav").hide();
+//    }else{
+//        $(".slide_nav").show();
+//    }
+//}
+//window.onscroll= function () {
+//    sro();
+//}
+var slide_nav=document.querySelector(".slide_nav");
 window.onscroll= function () {
-    var Top=document.body.scrollTop;
-    console.log(Top);
-    if(Top<390){
-        $(".slide_nav").hide();
+   var t=document.body.scrollTop;
+    if(t<390){
+        slide_nav.style.opacity=0;
     }else{
-        $(".slide_nav").show();
+        slide_nav.style.opacity=1;
     }
 }
